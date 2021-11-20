3791f37fde859ef7d6699243997a1f48f051c73d
Max K <Max-MKA@web.de>
2021-11-20 23:01:44 +0100

    Camera Streaming
    added support for displaying a video stream in the web-UI

---
+++ Raspberry Pi Software/start_stream.sh
@@ -0,0 +1,1 @@
+LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -vf -hf -fps 15 -q 50 -x 640 -y 480" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
